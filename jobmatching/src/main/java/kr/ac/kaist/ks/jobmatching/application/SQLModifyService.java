9a6f35e403bb3558c4ca395da7ba9d460db894a9
Seongchan Kim <kisckorea@gmail.com>
2015-09-01 17:16:52 +0900

    test

--- jobmatching/src/main/java/kr/ac/kaist/ks/jobmatching/application/SQLModifyService.java
+++ jobmatching/src/main/java/kr/ac/kaist/ks/jobmatching/application/SQLModifyService.java
@@ -1,7 +1,10 @@
 package kr.ac.kaist.ks.jobmatching.application;
 
+import java.util.ArrayList;
+
 import kr.ac.kaist.ks.jobmatching.domain.model.req.Req;
 import kr.ac.kaist.ks.jobmatching.domain.model.user.User;
+import kr.ac.kaist.ks.jobmatching.infrastructure.repository.MatchingTable;
 
 public class SQLModifyService {
 	private User user = null;
@@ -21,18 +24,46 @@ public class SQLModifyService {
 		String jobsNm1 = null;
 		String jobsNm2 = null;
 		
-		if(req.getJobsNm().equals("68205")){
-			jobsNm1 = "131903";
-			jobsNm2 = "156101";
+		
+
+		
+		String prefJobID= req.getJobsNm();
+		
+		MatchingTable mt = new  MatchingTable();
+		
+		int threshold =35;
+		ArrayList<String> result = mt.getSimilarJob(prefJobID, threshold);
+		
+		for (int i = 0 ;i<5; i++) {
+			System.out.println(result.get(i));
 		}
 		
+		
+		String[] ids = new String[result.size()];
+		
+		for(int i=0;i<result.size();i++){
+			String[] temp = result.get(i).split("\t");
+			ids[i] = temp[2];
+			
+		}
+					
+		modifiedReq = "select * from test1.comp where  (jobsNm like '%" + req.getJobsNm() +"%' or jobsNm like '%" + ids[0] +"%'"+
+		" or jobsNm like '%" + ids[1] +"%'"+" or jobsNm like '%" + ids[2] +"%'"+ " or jobsNm like '%" + ids[3] +"%'"+ " or jobsNm like '%" +  ids[4] + "%')";
+
 		
 		/*modifiedReq = "select * from test1.comp where busiSize = '" + req.getBusiSize() + "' and (jobsNm like '%" + req.getJobsNm() +"%' or jobsNm like '%" + jobsNm1 +"%'"
 				+ " or jobsNm like '%" + jobsNm2 + "%')";*/
 		
 		
-		modifiedReq = "select * from test1.comp where  (jobsNm like '%" + req.getJobsNm() +"%' or jobsNm like '%" + jobsNm1 +"%'"
-				+ " or jobsNm like '%" + jobsNm2 + "%')";
+ /*
+		 if(req.getJobsNm().equals("68205")){
+			jobsNm1 = "131903";
+			jobsNm2 = "156101";
+		}
+
+		modifiedReq = "select * from test1.comp where  (jobsNm like '%" + req.getJobsNm() +"%' or jobsNm like '%" + jobsNm1 +"%'"+ " or jobsNm like '%" + jobsNm2 + "%')";
+	*/			
+				
 		
 		
 		return modifiedReq;	
